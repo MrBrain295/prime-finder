d06bb8eb404ff28ee46a4e49e3ded5192b90e591
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2021-12-28 10:20:21 -0600

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import time
 
@@ -15,31 +18,26 @@ def primesfrom2to(n):
             sieve[(k*k+4*k-2*k*(i&1))//3::2*k] = False
     return np.r_[2,3,((3*np.nonzero(sieve)[0]+1)|1)]
  
-p = primes10m   = primesfrom2to(10_000_000)
-s = strong10m   = [t for s, t, u in zip(p, p[1:], p[2:]) 
+p = primes1b   = primesfrom2to(1_000)
+s = strong1b   = [t for s, t, u in zip(p, p[1:], p[2:]) 
                    if t > (s + u) / 2]
-w = weak10m     = [t for s, t, u in zip(p, p[1:], p[2:]) 
+w = weak1b     = [t for s, t, u in zip(p, p[1:], p[2:]) 
                    if t < (s + u) / 2]
-b = balanced10m = [t for s, t, u in zip(p, p[1:], p[2:]) 
+b = balanced1b = [t for s, t, u in zip(p, p[1:], p[2:]) 
                    if t == (s + u) / 2]
  
-print('The first 2000 strong primes:', s[:2000])
-print('The count of the strong primes below 100,000,000,000:',
-      sum(1 for p in s if p < 100_000_000_000))
-print('The count of the strong primes below  100,000,000,000:', len(s))
-print('\nThe first 2000 weak primes:', w[:2000])
-print('The count of the weak primes below 100,000,000,000:',
-      sum(1 for p in w if p < 100_000_000_000))
-print('The count of the weak primes below 100,000,000,000:', len(w))
-print('\n\nThe first 2000 balanced primes:', b[:2000])
-print('The count of balanced primes below 100,000,000,000:',
-      sum(1 for p in b if p < 100_000_000_000))
-print('The count of balanced primes below 100,000,000,000:', len(b))
-print('\nTOTAL primes below   100,000,000,000:',
-      sum(1 for pr in p if pr < 100_000_000_000))
-print('TOTAL primes below 100,000,000,000:', len(p))
+print('The first 100 strong primes:', s[:100])
+print('The count of the strong primes below 100:',
+      sum(1 for p in s if p < 100))
+print('The count of the strong primes below  100:', len(s))
+print('\nThe first 100 weak primes:', w[:100])
+print('The count of the weak primes below 100:',
+      sum(1 for p in w if p < 100))
+print('The count of the weak primes below 100:', len(w))
+print('\nThe first 1,000 balanced primes:', b[:100])
+print('The count of balanced primes below 1,000:', len(b))
+print('TOTAL primes below 1,000:', len(p))
 
 
 finish = time.perf_counter()
 print(f"Done in {finish - start:0.4f} seconds")
-
